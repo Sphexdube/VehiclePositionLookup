6361dbfaad1cfdf235b7b33f10c4e87848a01223
Sphexdube <se.dube28@gmail.com>
2022-11-26 12:50:44 +0200

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,19 @@
+# VehiclePositionLookup 
+
+### Tech Assessment
+
+### (C# .NET Core Console App)
+
+Nearest Vehicle Positions
+--------
+
+  This program finds the nearest vehicle position in the data file to each of the 10 co-ordinates provided. The 10 co-ordinates to find the closed vehicle positions to are as follows.
+  - Position 1 Latitude = 34.544909 Longitude = -102.100843
+  - Position 2 Latitude = 32.345544 Longitude = -99.123124
+  - Position 3 Latitude = 33.234235 Longitude = -100.214124
+  - Position 5 Latitude = 31.895839 Longitude = -97.789573
+  - Position 6 Latitude = 32.895839 Longitude = -101.789573
+  - Position 7 Latitude = 34.115839 Longitude = -100.225732
+  - Position 8 Latitude = 32.335839 Longitude = -99.992232
+  - Position 9 Latitude = 33.535339 Longitude = -94.792232
+  - Position 10 Latitude = 32.234235 Longitude = -100.222222
---
+++ VehiclePositionLookup/DataFileParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using VehiclePositionLookup.Utility;
+
+namespace VehiclePositionLookup
+{
+    public class DataFileParser
+    {
+        internal static List<VehiclePosition> ReadDataFile()
+        {
+            byte[] data = ReadFileData();
+            List<VehiclePosition> vehiclePositionList = new List<VehiclePosition>();
+            int offset = 0;
+            while (offset < data.Length)
+                vehiclePositionList.Add(ReadVehiclePosition(data, ref offset));
+            return vehiclePositionList;
+        }
+
+        internal static byte[] ReadFileData()
+        {
+            string localFilePath = UtilityMethods.GetLocalFilePath("VehiclePositions.dat");
+            if (File.Exists(localFilePath))
+                return File.ReadAllBytes(localFilePath);
+            Console.WriteLine("Data file not found.");
+            return (byte[])null;
+        }
+
+        private static VehiclePosition ReadVehiclePosition(byte[] data, ref int offset) => VehiclePosition.FromBytes(data, ref offset);
+    }
+}
