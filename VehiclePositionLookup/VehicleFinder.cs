9301a4f057087058829c0e8b9daab9f99023bbbd
Sphexdube <se.dube28@gmail.com>
2022-12-11 04:33:44 +0200

    Improved GetNearest menthod

--- VehiclePositionLookup/VehicleFinder.cs
+++ VehiclePositionLookup/VehicleFinder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using VehiclePositionLookup.Models;
 using VehiclePositionLookup.Utility;
 
@@ -11,7 +12,7 @@ namespace VehiclePositionLookup
         internal static void FindClosest(Coordinate[] coords)
         {
             List<VehiclePosition> vehiclePositionList = new List<VehiclePosition>();
-            
+
             Stopwatch stopwatch = Stopwatch.StartNew();
 
             List<VehiclePosition> vehiclePositions = DataFileParser.ReadDataFile();
@@ -22,21 +23,25 @@ namespace VehiclePositionLookup
 
             stopwatch.Restart();
 
-            foreach (Coordinate coord in coords)
+            Parallel.ForEach(coords, coord =>
             {
-                vehiclePositionList.Add(GetNearest(vehiclePositions, coord.Latitude, coord.Longitude, out double _));
-            }
+                vehiclePositionList.Add(GetNearest(vehiclePositions, coord.Latitude, coord.Longitude));
+            });
 
             stopwatch.Stop();
 
-            DisplayExecutionTimes(stopwatch, elapsedMilliseconds);
+            Console.WriteLine($"Data file read execution time : {(object)elapsedMilliseconds} ms");
+            Console.WriteLine($"Closest position calculation execution time : {(object)stopwatch.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Total execution time : {(object)(elapsedMilliseconds + stopwatch.ElapsedMilliseconds)} ms");
+            Console.WriteLine();
         }
 
-        internal static VehiclePosition GetNearest(List<VehiclePosition> vehiclePositions, float latitude, float longitude, out double nearestDistance)
+        internal static VehiclePosition GetNearest(List<VehiclePosition> vehiclePositions, float latitude, float longitude)
         {
-            VehiclePosition nearest = (VehiclePosition)null;
-            nearestDistance = double.MaxValue;
-            foreach (VehiclePosition vehiclePosition in vehiclePositions)
+            double nearestDistance = 0;
+            VehiclePosition nearest = null;
+
+            Parallel.ForEach(vehiclePositions, vehiclePosition => 
             {
                 double num = UtilityMethods.DistanceBetween(latitude, longitude, vehiclePosition.Latitude, vehiclePosition.Longitude);
                 if (num < nearestDistance)
@@ -44,16 +49,9 @@ namespace VehiclePositionLookup
                     nearest = vehiclePosition;
                     nearestDistance = num;
                 }
-            }
-            return nearest;
-        }
+            });
 
-        private static void DisplayExecutionTimes(Stopwatch stopwatch, long elapsedMilliseconds)
-        {
-            Console.WriteLine($"Data file read execution time : {(object)elapsedMilliseconds} ms");
-            Console.WriteLine($"Closest position calculation execution time : {(object)stopwatch.ElapsedMilliseconds} ms");
-            Console.WriteLine($"Total execution time : {(object)(elapsedMilliseconds + stopwatch.ElapsedMilliseconds)} ms");
-            Console.WriteLine();
+            return nearest;
         }
     }
 }
